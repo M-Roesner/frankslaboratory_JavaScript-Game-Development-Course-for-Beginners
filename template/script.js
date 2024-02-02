97c2bd544a906fb761012d2b2525618dda7531e5
M-Roesner <markus_roesner@web.de>
2024-02-02 18:04:25 +0100

    Adjust template to receive width and height automatically.

--- template/script.js
+++ template/script.js
@@ -4,5 +4,9 @@ const canvas = document.getElementById("canvas1");
 const ctx = canvas.getContext("2d");
 
 // width and height comes from the style.css properties
-const CANVAS_WIDTH = (canvas.width = 600);
-const CANVAS_HEIGT = (canvas.height = 600);
+const canvasStyle = window.getComputedStyle(canvas);
+const CANVAS_WIDTH = (canvas.width = parseInt(canvasStyle.width));
+const CANVAS_HEIGT = (canvas.height = parseInt(canvasStyle.height));
+console.log(`Canvas width: ${CANVAS_WIDTH}, Canvas height: ${CANVAS_HEIGT}`);
+
+window.addEventListener("load", () => {});
