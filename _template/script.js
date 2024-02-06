43004f9214efc5648a1cbfd74804feb0de2febf8
M-Roesner <markus_roesner@web.de>
2024-02-06 21:19:07 +0100

    Adjust template.

--- _template/script.js
+++ _template/script.js
@@ -1,15 +1,15 @@
 /** @type {HTMLCanvasElement} */
 
+const canvas = document.getElementById("canvas1");
+const ctx = canvas.getContext("2d");
+
+// The width and height of the canvas are automatically obtained from the style.css properties.
+const canvasStyle = window.getComputedStyle(canvas);
+const CANVAS_WIDTH = (canvas.width = parseInt(canvasStyle.width));
+const CANVAS_HEIGT = (canvas.height = parseInt(canvasStyle.height));
+console.log(`Canvas width: ${CANVAS_WIDTH}, Canvas height: ${CANVAS_HEIGT}`);
+
 window.addEventListener("load", () => {
-  const canvas = document.getElementById("canvas1");
-  const ctx = canvas.getContext("2d");
-
-  // The width and height of the canvas are automatically obtained from the style.css properties.
-  const canvasStyle = window.getComputedStyle(canvas);
-  const CANVAS_WIDTH = (canvas.width = parseInt(canvasStyle.width));
-  const CANVAS_HEIGT = (canvas.height = parseInt(canvasStyle.height));
-  console.log(`Canvas width: ${CANVAS_WIDTH}, Canvas height: ${CANVAS_HEIGT}`);
-
   class Classname {
     constructor() {}
     update(deltaTime) {}
