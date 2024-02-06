7c60cc290f072fff3ccb2d5526e91f97187a0f1c
M-Roesner <markus_roesner@web.de>
2024-02-06 13:45:26 +0100

    Adjust starter template

--- _template/script.js
+++ _template/script.js
@@ -1,17 +1,27 @@
 /** @type {HTMLCanvasElement} */
 
-const canvas = document.getElementById("canvas1");
-const ctx = canvas.getContext("2d");
-
-// The width and height of the canvas are automatically obtained from the style.css properties.
-const canvasStyle = window.getComputedStyle(canvas);
-const CANVAS_WIDTH = (canvas.width = parseInt(canvasStyle.width));
-const CANVAS_HEIGT = (canvas.height = parseInt(canvasStyle.height));
-console.log(`Canvas width: ${CANVAS_WIDTH}, Canvas height: ${CANVAS_HEIGT}`);
-
 window.addEventListener("load", () => {
-  const animate = () => {
+  const canvas = document.getElementById("canvas1");
+  const ctx = canvas.getContext("2d");
+
+  // The width and height of the canvas are automatically obtained from the style.css properties.
+  const canvasStyle = window.getComputedStyle(canvas);
+  const CANVAS_WIDTH = (canvas.width = parseInt(canvasStyle.width));
+  const CANVAS_HEIGT = (canvas.height = parseInt(canvasStyle.height));
+  console.log(`Canvas width: ${CANVAS_WIDTH}, Canvas height: ${CANVAS_HEIGT}`);
+
+  class Classname {
+    constructor() {}
+    update(deltaTime) {}
+    draw() {}
+  }
+
+  let lastTime = 0;
+  const animate = (timestamp) => {
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
+    let deltaTime = timestamp - lastTime;
+    lastTime = timestamp;
     requestAnimationFrame(animate);
   };
-  animate();
+  animate(0);
 });
