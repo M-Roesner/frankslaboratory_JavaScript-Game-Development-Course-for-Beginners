2965b1468db7efafc6d6b3974babc72072511d56
M-Roesner <markus_roesner@web.de>
2024-02-07 18:43:34 +0100

    Adjust template

--- _template/script.js
+++ _template/script.js
@@ -5,9 +5,9 @@ const ctx = canvas.getContext("2d");
 
 // The width and height of the canvas are automatically obtained from the style.css properties.
 const canvasStyle = window.getComputedStyle(canvas);
-const CANVAS_WIDTH = (canvas.width = parseInt(canvasStyle.width));
-const CANVAS_HEIGT = (canvas.height = parseInt(canvasStyle.height));
-console.log(`Canvas width: ${CANVAS_WIDTH}, Canvas height: ${CANVAS_HEIGT}`);
+canvas.width = parseInt(canvasStyle.width);
+canvas.height = parseInt(canvasStyle.height);
+console.log(`Canvas width: ${canvas.height}, Canvas height: ${canvas.width}`);
 
 window.addEventListener("load", () => {
   class Classname {
@@ -21,6 +21,9 @@ window.addEventListener("load", () => {
     ctx.clearRect(0, 0, canvas.width, canvas.height);
     let deltaTime = timestamp - lastTime;
     lastTime = timestamp;
+
+    // code
+
     requestAnimationFrame(animate);
   };
   animate(0);
