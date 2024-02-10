69307990dac49338418fe5980d0aa2bdbaa8745e
M-Roesner <markus_roesner@web.de>
2024-02-10 08:24:22 +0100

    Set initial settings for part 10.

--- Part10-Final-endless-running-game/main.js
+++ Part10-Final-endless-running-game/main.js
@@ -2,20 +2,11 @@
 
 const canvas = document.getElementById("canvas1");
 const ctx = canvas.getContext("2d");
-
-// The width and height of the canvas are automatically obtained from the style.css properties.
-const canvasStyle = window.getComputedStyle(canvas);
-canvas.width = parseInt(canvasStyle.width);
-canvas.height = parseInt(canvasStyle.height);
+canvas.width = 500;
+canvas.height = 500;
 console.log(`Canvas width: ${canvas.height}, Canvas height: ${canvas.width}`);
 
 window.addEventListener("load", () => {
-  class Classname {
-    constructor() {}
-    update(deltaTime) {}
-    draw() {}
-  }
-
   let lastTime = 0;
   const animate = (timestamp) => {
     ctx.clearRect(0, 0, canvas.width, canvas.height);
